074d7072fbd47d6306fc15f07eaf573dac6a5f86
Clive B Harrison <clive_b_harrison@outlook.com>
2021-11-12 22:12:04 +0000

    Add project files.

---
+++ Floating Point Math CBH/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnFloatMath_Click(sender As Object, e As EventArgs) Handles btnFloatMath.Click
+        'Declare variable
+        Dim dblNumber As Double
+
+        'Set number, multiply numbers, and display results
+        dblNumber = 45.34
+        dblNumber *= 4.333
+        MessageBox.Show("Multiplication test. " & dblNumber.ToString,
+        "Floating Points")
+
+        'Set number, divide numbers, and display results
+        dblNumber = 12000
+        dblNumber /= 7
+
+        'Display the results without formatting
+        MessageBox.Show("Division test without formatting. " &
+            dblNumber.ToString, "Floating Points")
+
+        'Display the results with formatting
+        MessageBox.Show("Division test with formatting. " &
+            String.Format("{0:n3}", dblNumber), "Floating Points")
+
+        MessageBox.Show("Division test with formatting. CBH " &
+            String.Format("{2:n3}", 0.123456789, 1.23456789, 2.34567891), "Floating Points")
+    End Sub
+End Class
---
+++ Floating Point Math/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub btnFloatMath_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles btnFloatMath.Click
+        'Delcare variable
+        Dim dblNumber As Double
+
+        'Set number, multiply numbers, and display results
+        dblNumber = 45.34
+        dblNumber *= 4.333
+        MessageBox.Show("Multiplication test.. " & dblNumber.ToString, _
+            "Floating Points")
+
+        'Set number, divide numbers, and display results
+        dblNumber = 12
+        dblNumber /= 7
+
+        'Display the results without formatting
+        MessageBox.Show("Division test without formatting.. " & _
+            dblNumber.ToString, "Floating Points")
+
+        'Display the results with formatting
+        MessageBox.Show("Division test with formatting.. " & _
+            String.Format("{0:n3}", dblNumber), "Floating Points")
+    End Sub
+End Class
